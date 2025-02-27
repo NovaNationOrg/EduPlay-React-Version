7b98bbfcb5ecc084864542be8d10a7e39549106b
che.salazar <che.salazar@my.uwi.edu>
2025-02-26 23:31:25 -0400

    Initial Jeopardy Game Implementation

---
+++ src/pages/Jeopardy/list-page.tsx
@@ -0,0 +1,47 @@
+import { useNavigate } from "react-router-dom";
+import "../../styles/Jeopardy/list-page.css"
+import Header from '../../components/header'
+import ListData from "../../components/dynamic-list";
+
+
+function removeCategory() {
+    sessionStorage.removeItem("category")
+}
+
+function removeTier() {
+    sessionStorage.removeItem("tier")
+}
+
+export default function ListPage() {
+    const navigate = useNavigate()
+
+    return (
+        <>
+            <div className="panel">
+                <Header gameClass="hangman-header" headerText="Jeopardy" />
+
+                {sessionStorage.getItem("category") == null ? (
+                    <>
+                        <button className="return-button" onClick={() => {
+                            removeCategory();
+                            navigate("/")
+                        }}>QUIT</button>
+                        <header className="heading-text">Select a Category</header>
+                        <ListData type="Category" />
+                    </>
+                ) : (
+                    <>
+                        <button className="return-button" onClick={() => {
+                            navigate("/jeopardyGame")
+                            removeTier()
+                            removeCategory()
+                        }}>QUIT</button>
+                        <header className="heading-text">Select a Tier</header>
+                        <ListData type="Tier" />
+                    </>
+                )}
+
+            </div >
+        </>
+    )
+}
