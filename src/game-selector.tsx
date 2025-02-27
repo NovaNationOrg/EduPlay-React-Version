46350b40289b81ab015a635372748914b6a6ce6a
che.salazar <che.salazar@my.uwi.edu>
2025-02-26 23:28:44 -0400

    Cleaned Up Code

--- src/game-selector.tsx
+++ src/game-selector.tsx
@@ -2,7 +2,7 @@ import loadJeopardy from "./components/jeopardy-loader";
 
 export function gameSelector(gameData:string){
 
-    let splitData = gameData.split("\n")
+    const splitData = gameData.split("\n")
     if(splitData[0]=="_jp_")
         loadJeopardy(splitData)
 }   
