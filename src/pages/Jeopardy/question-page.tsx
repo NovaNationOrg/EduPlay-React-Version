a5054c10507def0df021fa5930f87326f4a1f675
Flaxere <123189890+Flaxere@users.noreply.github.com>
2025-03-05 02:37:18 -0400

    implemented countdown action and initial guess validation functionality

--- src/pages/Jeopardy/question-page.tsx
+++ src/pages/Jeopardy/question-page.tsx
@@ -1,20 +1,15 @@
-import { useNavigate } from "react-router-dom";
+import { NavigateFunction, useNavigate } from "react-router-dom";
 import Header from "../../components/header";
 import "../../styles/Jeopardy/question-page.css";
 import { useState, useEffect } from "react";
 import { useLiveQuery } from "dexie-react-hooks";
 import { db } from "../../database/db";
+import { nav } from "motion/react-client";
 
-function getQuestion() {
-    const tier = Number(sessionStorage.getItem("tier"))
-    const cat = sessionStorage.getItem("category")
-
-    // eslint-disable-next-line react-hooks/rules-of-hooks
-    const jeopardyGameData = useLiveQuery(() => db.jeopardyData
-        .where({ points: tier, theme: cat }).toArray())
-
-    const question: string[] = [...new Set(jeopardyGameData?.map(record => record.question))]
-    return question[0]
+function pageReturn(navigate:NavigateFunction){
+    removeTier()
+    removeCategory()
+    navigate(-1)
 }
 
 function removeTier() {
@@ -25,15 +20,15 @@ function removeCategory() {
     sessionStorage.removeItem("category")
 }
 
-function submit() {
-
-}
-
 const CountdownTimer = () => {
     const [timeLeft, setTimeLeft] = useState(60);
-
+    const navigate= useNavigate()
     useEffect(() => {
-        if (timeLeft <= 0) return;
+        if (timeLeft <= 0) {
+            pageReturn(navigate)
+            return;
+        }
+           
 
         const timer = setInterval(() => {
             setTimeLeft((prevTime) => prevTime - 1);
@@ -46,10 +41,31 @@ const CountdownTimer = () => {
 };
 
 export default function QuestionPage() {
+    const [guess,setGuess] = useState("")
     const navigate = useNavigate()
-    const points = sessionStorage.getItem("tier")
 
-    const question = getQuestion()
+    function submit() {
+        if(guess==answer)
+            console.log("Victory")
+        else
+            console.log(answer)
+    }
+
+    const tier = Number(sessionStorage.getItem("tier"))
+    const cat = sessionStorage.getItem("category")
+
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    const jeopardyGameData = useLiveQuery(() => db.jeopardyData
+        .where({ points: tier, theme: cat }).toArray())
+
+    if(jeopardyGameData==null)
+        return
+    
+    const question = jeopardyGameData[0].question
+    const answer = jeopardyGameData[0].answer
+    const points = jeopardyGameData[0].points
+
+
     return (
         <>
             <div className="panel">
@@ -57,16 +73,14 @@ export default function QuestionPage() {
                 <div className="info-panel">
                     <p className="timer"><CountdownTimer /></p>
                     <button className="return-button" onClick={() => {
-                        removeTier()
-                        removeCategory()
-                        navigate(-1)
+                       pageReturn(navigate)
                     }}>QUIT</button>
                 </div>
                 <header className="heading-text">Question for {points} Points!</header>
                 <div className="question-area">
                     <header className="question-text">{question}</header>
                 </div>
-                <input className="answer-area" placeholder="ENTER YOUR ANSWER"></input>
+                <input className="answer-area" placeholder="ENTER YOUR ANSWER" onChange={(e) => setGuess(e.target.value)}></input> {/*onChange={(e) => setGuess(e.target.value)}*/}
                 <button className="submit-button" onClick={() => {
                     submit()
                 }}>SUBMIT</button>
