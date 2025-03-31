a50e3fac8bca23b31c30dedc38921634068be15c
Flaxere <123189890+Flaxere@users.noreply.github.com>
2025-03-31 02:00:11 -0400

    Updated the IScanner Components to include zoom

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -85,7 +85,7 @@ define(['./workbox-0bd21846'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.96e744qsrso"
+    "revision": "0.t2pvft7ema"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
--- src/components/qrSettings.tsx
+++ src/components/qrSettings.tsx
@@ -3,4 +3,5 @@ import { IScannerComponents } from '@yudiel/react-qr-scanner';;
 export const qrComponents: IScannerComponents = {
     audio: false,
     finder: true,
+    zoom:true
 }
