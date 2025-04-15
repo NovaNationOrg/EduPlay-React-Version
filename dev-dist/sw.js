83cce0633e9b65043ad4646a3140d3c49e1ba3b8
Flaxere <123189890+Flaxere@users.noreply.github.com>
2025-04-15 01:32:10 -0400

    added potential fix in resupporting offline fonts

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -81,11 +81,8 @@ define(['./workbox-99d8380f'], (function (workbox) { 'use strict';
    * See https://goo.gl/S9QRab
    */
   workbox.precacheAndRoute([{
-    "url": "registerSW.js",
-    "revision": "3ca0b8505b4bec776b69afdba2768812"
-  }, {
     "url": "index.html",
-    "revision": "0.b8vdrdgqhdg"
+    "revision": "0.sp6hmcfrd3o"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,67 @@ import { ManifestOptions, VitePWA, VitePWAOptions } from 'vite-plugin-pwa'
 import {fileURLToPath, URL } from 'node:url'
 import replace from '@rollup/plugin-replace'
 
+// https://vite.dev/config/
+// export default defineConfig({
+  
+//   plugins: [
+//     react(),
+//     VitePWA({ 
+//       injectRegister: 'script',
+//       manifest:{
+        
+//         icons:[
+//           {
+//           "src": "/web-app-manifest-192x192.png",
+//           "sizes": "192x192",
+//           "type": "image/png",
+//           "purpose": "maskable"
+//         },
+//         {
+//           "src": "/web-app-manifest-512x512.png",
+//           "sizes": "512x512",
+//           "type": "image/png",
+//           "purpose": "maskable"
+//         }
+//       ],
+//         theme_color:"#181818",
+//       },
+//       registerType: 'autoUpdate',
+//       devOptions:{
+//         enabled:true
+//       } ,
+//       workbox:{
+//         globPatterns: ['**/*.{js,ts,css,html,ico,png,svg,wasm,ttf}'],
+        
+//         runtimeCaching:[
+//           {
+//           urlPattern: ({ url }) => {
+//               return url.pathname.startsWith("/npm/zxing-wasm@2.1.0/dist/reader/zxing_reader.wasm")
+//           },
+//           handler: "CacheFirst" as const,
+//           options:{   
+//             cacheName: "zxing-cache",
+//             cacheableResponse: {
+//               statuses: [0,200]
+//             },
+//             expiration:{
+//               maxEntries:1,
+//             }
+//           }
+//         },
+//         ]
+//       }
+
+//     }
+//     )
+//   ], resolve: {
+//     alias: {
+//       '@': fileURLToPath(new URL('./src', import.meta.url))
+//     }
+//   }
+  
+  
+// })
 
 const replaceOptions = { __DATE__: new Date().toISOString() }
 const claims = process.env.CLAIMS === 'true'
@@ -24,6 +85,9 @@ const pwaOptions: Partial<VitePWAOptions> = {
     theme_color:"#181818",
   },
   registerType: 'prompt',
+  workbox:{
+    globPatterns: ['**/*.{ttf}']
+  },
   devOptions:{
     enabled:true
   } ,
