959ba9a677924610c68f700ba6d5969c4ea0c9f7
Flaxere <123189890+Flaxere@users.noreply.github.com>
2025-04-15 06:26:10 -0400

    Updated the reload prompt to remove the unexpected elemnts

--- src/components/reload-prompt.tsx
+++ src/components/reload-prompt.tsx
@@ -31,25 +31,17 @@ function ReloadPrompt() {
         setOfflineReady(false)
         setNeedRefresh(false)
         }
-    console.log(needRefresh)
-    return (
-        <div>
-        { (offlineReady || needRefresh)
-            && <div>
-                { offlineReady &&(
-                    toast.info("App ready to work offline",{id:"offline-ready-toast"})
-                )
-                }
-       
-                { needRefresh &&  toast.info("New content available, click the reload button to update", {
-                id:"reload-prompt-toast", duration:Infinity, dismissible:false,
-                action : {label:"Reload",key:"no-button",onClick: () =>{updateServiceWorker(true)}},
-                cancel : {label:"Close",key:"yes-button",onClick: () =>{close()}}
-                }) }
-            </div>
-        }
-        </div>
-    )
+    
+    if(offlineReady)
+        toast.info("App ready to work offline",{id:"offline-ready-toast"})
+    if(needRefresh)
+        toast.info("New content available, click on reload button to update", {
+            id:"reload-prompt-toast", duration:Infinity, dismissible:false,
+            action : {label:"Reload",key:"no-button",onClick: () =>{updateServiceWorker(true)}},
+            cancel : {label:"Close",key:"yes-button",onClick: () =>{close()}}
+        }) 
+
+    return(<></>)
 }
 
 
