8e5f42d58aeba7ed0798c2c44827834b4ba2ac67
Flaxere <123189890+Flaxere@users.noreply.github.com>
2025-04-15 00:59:17 -0400

    Added PWA  lifecycle update implementation

---
+++ src/components/reload-prompt.tsx
@@ -0,0 +1,58 @@
+import { toast, Toaster } from 'sonner'
+
+import { useRegisterSW } from 'virtual:pwa-register/react'
+
+function ReloadPrompt() {
+    const reloadSW = '__RELOAD_SW__'
+    const {
+        offlineReady: [offlineReady, setOfflineReady],
+        needRefresh: [needRefresh, setNeedRefresh],
+        updateServiceWorker,
+    } = useRegisterSW({
+        onRegisteredSW(swUrl, r) {
+        console.log(`Service Worker at: ${swUrl}`)
+        // @ts-expect-error just ignore
+        if (reloadSW === 'true') {
+            r && setInterval(() => {
+            console.log('Checking for sw update')
+            r.update()
+            }, 1800000 /* 20s for testing purposes */)
+        } 
+        else {
+            // eslint-disable-next-line prefer-template
+            console.log('SW Registered: ' + r)
+        }
+        },
+        onRegisterError(error) {
+        console.log('SW registration error', error)
+        },
+    })
+    const close = () => {
+        setOfflineReady(false)
+        setNeedRefresh(false)
+        }
+    console.log(needRefresh)
+    return (
+        <div>
+            <Toaster richColors position='top-center'/>
+        { (offlineReady || needRefresh)
+            && <div>
+                { offlineReady &&(
+                    toast.info("App ready to work offline",{id:"offline-ready-toast"})
+                )
+                }
+       
+                { needRefresh &&  toast.info("New content available, click the reload button to update", {
+                id:"reload-prompt-toast", duration:Infinity, dismissible:false,
+                action : {label:"Reload",key:"no-button",onClick: () =>{updateServiceWorker(true)}},
+                cancel : {label:"Close",key:"yes-button",onClick: () =>{close()}}
+                }) }
+            </div>
+        }
+        </div>
+    )
+}
+
+
+
+export default ReloadPrompt
